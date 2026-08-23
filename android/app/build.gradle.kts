a395103109f444a5f165c821051886290cb9bc36
PinMind <pinmind@local>
2026-08-23 12:44:05 +0800

    Add dynamic note filters and table of contents

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -7,7 +7,7 @@ android {
         applicationId = "com.pinmind.beta"
         minSdk = 24
         targetSdk = 35
-        versionCode = 40
-        versionName = "0.6.7"
+        versionCode = 41
+        versionName = "0.6.8"
     }
 }
